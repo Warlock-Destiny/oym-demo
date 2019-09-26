63698796d45099ba8b25532c0e9f458f3b997853
5zyd14 <1571792921@qq.com>
2019-09-26 17:32:59 +0800

    1 修改因为base包生成产生的报错

--- zyd-demo-activiti/src/main/java/com/cn/zyd/activiti/dao/ActReProcdefDao.java
+++ zyd-demo-activiti/src/main/java/com/cn/zyd/activiti/dao/ActReProcdefDao.java
@@ -1,7 +1,7 @@
 package com.cn.zyd.activiti.dao;
 
-import com.cn.zyd.base.zyd.common.db.BaseDao;
 import com.cn.zyd.activiti.entity.ActReProcdef;
+import com.cn.zyd.base.BaseDao;
 
 public interface ActReProcdefDao extends BaseDao<ActReProcdef> {
 }
