4420326872747a6c64bcea2495cfe2498af9534e
5zyd14 <zhangyangdi14>
2019-11-23 23:23:32 +0800

    提交

--- zyd-demo-mq/zyd-demo-mq-base/src/main/java/com/cn/zyd/mq/BaseConsumer.java
+++ zyd-demo-mq/zyd-demo-mq-base/src/main/java/com/cn/zyd/mq/BaseConsumer.java
@@ -9,7 +9,6 @@ import java.util.function.Consumer;
 /**
  * @author zyd
  * @date 2019/11/21 11:04
- * @desc
  */
 public interface BaseConsumer<S extends MqSuit, C extends MqCallback> extends BaseMqClient {
 
