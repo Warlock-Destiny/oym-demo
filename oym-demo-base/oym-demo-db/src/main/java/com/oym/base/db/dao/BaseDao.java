154aa42475030cc62e21445772d73070520d2e4e
e <1571792921@qq.com>
2020-10-21 13:56:40 +0800

    1 补充少添加的base到数据

--- oym-demo-base/oym-demo-db/src/main/java/com/oym/base/db/dao/BaseDao.java
+++ oym-demo-base/oym-demo-db/src/main/java/com/oym/base/db/dao/BaseDao.java
@@ -1,7 +1,11 @@
 package com.oym.base.db.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.oym.base.db.entity.BaseEntity;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * @author zhangyd
@@ -9,4 +13,15 @@ import com.oym.base.db.entity.BaseEntity;
  * @desc
  */
 public interface BaseDao<T extends BaseEntity> extends BaseMapper<T> {
+    /**
+     * 根据 ID 修改
+     *
+     * @param entity 实体对象
+     */
+    int updateAllById(@Param(Constants.ENTITY) T entity);
+
+    /**
+     * 批量插入
+     */
+    void batchInsert(List<T> list);
 }
