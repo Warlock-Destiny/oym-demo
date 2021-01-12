efdec7ab207bc1dcd4d230b248c4c9d83d0b41cc
ydzhang <1571792921@qq.com>
2021-01-12 14:00:32 +0800

    修改dockerfile配置

--- oym-demo-others/oym-demo-docker/src/main/java/com/oym/DockerApplication.java
+++ oym-demo-others/oym-demo-docker/src/main/java/com/oym/DockerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZhangydDemoWebApplication {
+public class DockerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZhangydDemoWebApplication.class, args);
+        SpringApplication.run(DockerApplication.class, args);
     }
 
 }
